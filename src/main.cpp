611e753b885ee3ce086cbb85600ba8d000dab82e
Kevin Solorio <kevin.solorio@hey.com>
2020-07-31 21:44:37 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "number of args: " << argc << std::endl;
+    for (int i = 0; i < argc; i++) {
+        std::cout << "args " << i << ": " << argv[i] << std::endl;
+    }
+
+    return 0;
+}
