ac14d819347022d82cc1c40d973bfcef46ca9342
Kevin Solorio <kevin.solorio@hey.com>
2020-08-04 21:17:10 -0500

    begin key model

---
+++ include/key.h
@@ -0,0 +1,24 @@
+#ifndef KEY_H
+#define KEY_H
+
+#include <string>
+
+namespace vigenere
+{
+class Key
+{
+public:
+    // constructor
+    Key(const std::string &key);
+
+    // getters
+    std::string NormalizedKey() const;
+
+private:
+    const std::string &_raw_key;
+    std::string _normalized_key;
+};
+} // namespace vigenere
+
+
+#endif
---
+++ src/key.cpp
@@ -0,0 +1,18 @@
+#include "key.h"
+
+#include <cctype>
+
+namespace vigenere
+{
+    Key::Key(const std::string &key) : _raw_key(key)
+    {
+        _normalized_key = "";
+        for(auto letter : key)
+            if (isalpha(letter)) _normalized_key.push_back(tolower(letter));
+    };
+
+    std::string Key::NormalizedKey() const
+    {
+        return _normalized_key;
+    }
+} // namespace vigenere
