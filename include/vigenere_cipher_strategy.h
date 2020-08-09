1afedfeb6daa27eb6fc7bf473f7c69acb8e80b69
Kevin Solorio <kevin.solorio@hey.com>
2020-08-08 21:40:56 -0500

    add countes for meta information

--- include/vigenere_cipher_strategy.h
+++ include/vigenere_cipher_strategy.h
@@ -15,6 +15,10 @@ public:
     VigenereCipherStrategy(Key key, std::string &file);
 
 private:
+    int _enciphered_characters_count {0};
+    int _passthrough_characters_count {0};
+    int _skipped_characters_count {0};
+
     Key _key;
     std::string &_file;
 };
